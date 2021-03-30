dabfd0dfd233094e42a696615d09cfebe3f9df62
sungwoo park <ranmaru74@gmail.com>
2021-03-30 11:19:08 +0900

    Add humidity.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, 'It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees')
+            callback(undefined, 'It is currently ' + body.current.temperature + ' degrees out. It feels like ' +
+                body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%.')
         }
     })
 }
